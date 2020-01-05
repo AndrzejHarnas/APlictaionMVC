c48e0b5a65b31f69b249fccd8c1f27962f30a74e
Harnas_20 <harnas_20@o2.pl>
2020-01-05 11:09:40 +0100

    Dodaj pliki projektów.

---
+++ APlictaionMVC/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APlictaionMVC.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+
+    }
+}
