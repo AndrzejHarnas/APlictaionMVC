1177e21d417e01fcd1eb7eb342acae96baf9fea0
Harnas_20 <harnas_20@o2.pl>
2020-01-06 12:52:57 +0100

    simple routing

--- APlictaionMVC/App_Start/RouteConfig.cs
+++ APlictaionMVC/App_Start/RouteConfig.cs
@@ -13,6 +13,13 @@ namespace APlictaionMVC
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "WebApi",
+                url: "api/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
